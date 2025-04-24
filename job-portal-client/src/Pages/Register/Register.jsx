97164fb0d380a566fc3b271abd35465ffe65e9c8
najmul <mdnajmulislam10992@gmail.com>
2025-04-25 00:55:19 +0600

    lottie react and firebase setup

--- job-portal-client/src/Pages/Register/Register.jsx
+++ job-portal-client/src/Pages/Register/Register.jsx
@@ -1,25 +1,32 @@
 import React from "react";
-
+import Lottie from "lottie-react";
+import registerLottieData from '../../assets/Lottie/register.json'
 const Register = () => {
+    const handleRegister=(e)=>{
+        e.preventDefault()
+        const form = e.target;
+        const email = form.email.value
+        const password = form.password.value
+        console.log({email,password})
+        // pass validation
+
+        
+    }
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-         
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+        <div className="text-center lg:text-left w-96">
+         <Lottie animationData={registerLottieData}></Lottie>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
         <h1 className= "ml-8 mt-4 text-5xl font-bold">Register now!</h1>
-          <form className="card-body">
+          <form onSubmit={handleRegister} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
+              name="email"
                 type="email"
                 placeholder="email"
                 className="input input-bordered"
@@ -31,6 +38,7 @@ const Register = () => {
                 <span className="label-text">Password</span>
               </label>
               <input
+              name="password"
                 type="password"
                 placeholder="password"
                 className="input input-bordered"
