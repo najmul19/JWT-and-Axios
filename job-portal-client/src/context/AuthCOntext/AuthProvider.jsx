1594ea5f8136ff07376addd5d38a37cfa8cb5138
najmul <mdnajmulislam10992@gmail.com>
2025-04-25 01:12:26 +0600

    onstatechange

--- job-portal-client/src/context/AuthCOntext/AuthProvider.jsx
+++ job-portal-client/src/context/AuthCOntext/AuthProvider.jsx
@@ -1,24 +1,34 @@
-import React, { useState } from 'react';
-import AuthContext from './AuthContext';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../../Firebase/firebase.init';
+import React, { useEffect, useState } from "react";
+import AuthContext from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { auth } from "../../Firebase/firebase.init";
 
-const AuthProvider = ({children}) => {
-    const [user,setUser] = useState(null)
-    const [loading,setLoading] = useState(true)
-    const creatiUser=(email,password)=>{
-        setLoading(true)
-        return createUserWithEmailAndPassword(auth,email,password);
-    }
-    const authInfo = {
-        creatiUser,
-    }
-    
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const creatiUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const authInfo = {
+    creatiUser,
+  };
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log("state captured", currentUser)
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
