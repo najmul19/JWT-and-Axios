38f7ca5a95c25a9c29f7e73c4f876a9cd613c9b8
najmul <mdnajmulislam10992@gmail.com>
2025-04-25 00:30:02 +0600

    rout setup and folder structured

--- job-portal-client/src/router/router.jsx
+++ job-portal-client/src/router/router.jsx
@@ -1,9 +1,23 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
+import Home from "../Pages/Home/Home";
+import MainLayout from "../Layout/MainLayout";
+import Register from "../Pages/Register/Register";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>
+    element: <MainLayout></MainLayout>,
+    errorElement: <h2>Route not FOund</h2>,
+    children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+        {
+            path: '/register',
+            element: <Register></Register>
+        }
+    ]
   },
 ]);
 export default router
