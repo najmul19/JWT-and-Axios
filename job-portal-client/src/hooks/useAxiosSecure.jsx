52fc7ccb37b3ce0b3a807b315272def45269f040
najmul <mdnajmulislam10992@gmail.com>
2025-04-30 21:42:34 +0600

    auth interceptor using asios for bad user found then logout user and though the login page

--- job-portal-client/src/hooks/useAxiosSecure.jsx
+++ job-portal-client/src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,34 @@
 import axios from "axios";
-
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5000",
   withCredentials: true,
 });
 const useAxiosSecure = () => {
+  const { signOutUser } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    axiosInstance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log("error caught in interceptor");
+        if (error.status === 401 || error.status === 403) {
+          console.log("need to logout the user");
+          signOutUser()
+            .then(() => {
+              console.log("loged out user");
+              navigate("/signin");
+            })
+            .catch((e) => console.log(e));
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, []);
   return axiosInstance;
 };
 
